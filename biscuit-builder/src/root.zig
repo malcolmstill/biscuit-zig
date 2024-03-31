6eac9924d1a0b539bb13f2bf38efd689156a160d
Malcolm Still <malcolm.still@gmail.com>
2024-03-31 19:56:46 +0100

    WIP

--- biscuit-builder/src/root.zig
+++ biscuit-builder/src/root.zig
@@ -8,4 +8,5 @@ pub const Scope = @import("scope.zig").Scope;
 pub const Date = @import("date.zig").Date;
 pub const Policy = @import("policy.zig").Policy;
 pub const Parser = @import("parser.zig").Parser;
+pub const Block = @import("block.zig").Block;
 pub const Set = @import("biscuit-datalog").Set;
